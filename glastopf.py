9eb3e2e1b406f23e8c3a20c5db18425872ecb8c5
Lukas Rist <glaslos@gmail.com>
2011-09-25 07:14:37 +0000

    TABs converted to 4 spaces

--- glastopf.py
+++ glastopf.py
@@ -6,31 +6,31 @@ import modules.reporting.log_sqlite as log_sqlite
 
 class GlastopfHoneypot(object):
 
-	def __init__(self):
-		self.sqlite_logger = log_sqlite.LogSQLite()
+    def __init__(self):
+        self.sqlite_logger = log_sqlite.LogSQLite()
 
-	def print_info(self, attack_event):
-		print attack_event.event_time,
-		print attack_event.source_addr[0] + " requested",
-		print attack_event.parsed_request.method, 
-		print attack_event.parsed_request.url, "on", 
-		print attack_event.parsed_request.header["Host"]
-	
-	def handle_request(self, raw_request, addr):
-		HTTP_parser = util.HTTPParser()
-		attack_event = attack.AttackEvent()
-		# Parse the request
-		attack_event.parsed_request = HTTP_parser.parse_request(raw_request)
-		attack_event.source_addr = addr
-		self.print_info(attack_event)
-		# Start response with the server header
-		# TODO: Add msg length header
-		attack_event.response = util.HTTPServerResponse.response_header
-		MethodHandlers = method_handler.HTTPMethods()
-		# Handle the HTTP request method
-		attack_event.matched_pattern = getattr(MethodHandlers, attack_event.parsed_request.method, "GET")(attack_event.parsed_request)
-		# Handle the request with the specific vulnerability module
-		getattr(request_handler, attack_event.matched_pattern, request_handler.unknown)(attack_event)
-		# Logging the event
-		self.sqlite_logger.insert(attack_event)
-		return attack_event.response
+    def print_info(self, attack_event):
+        print attack_event.event_time,
+        print attack_event.source_addr[0] + " requested",
+        print attack_event.parsed_request.method, 
+        print attack_event.parsed_request.url, "on", 
+        print attack_event.parsed_request.header["Host"]
+    
+    def handle_request(self, raw_request, addr):
+        HTTP_parser = util.HTTPParser()
+        attack_event = attack.AttackEvent()
+        # Parse the request
+        attack_event.parsed_request = HTTP_parser.parse_request(raw_request)
+        attack_event.source_addr = addr
+        self.print_info(attack_event)
+        # Start response with the server header
+        # TODO: Add msg length header
+        attack_event.response = util.HTTPServerResponse.response_header
+        MethodHandlers = method_handler.HTTPMethods()
+        # Handle the HTTP request method
+        attack_event.matched_pattern = getattr(MethodHandlers, attack_event.parsed_request.method, "GET")(attack_event.parsed_request)
+        # Handle the request with the specific vulnerability module
+        getattr(request_handler, attack_event.matched_pattern, request_handler.unknown)(attack_event)
+        # Logging the event
+        self.sqlite_logger.insert(attack_event)
+        return attack_event.response
