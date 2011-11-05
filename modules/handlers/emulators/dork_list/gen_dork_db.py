6453b7aca7cf324e359e4fab9d510d2298de58cf
Guesslin <guesslin1986@gmail.com>
2011-11-05 07:18:12 +0000

    Fin with DDL(dymanic dork list) DB form.

--- modules/handlers/emulators/dork_list/gen_dork_db.py
+++ modules/handlers/emulators/dork_list/gen_dork_db.py
@@ -1,16 +1,29 @@
 import sqlite3
 
 class LogDork(object): 
+
     def __init__(self):
 		try:
 			self.conn = sqlite3.connect("dork.db")
 		except sqlite3.OperationalError: # can't locate database
+			print "Please locate dork.db file"
 			exit(1)
 		self.cursor = self.conn.cursor()
+		self.create()
 
 	def create(self):
-		
+		tablename = ["intitle", "inttext", "inurl", "filetype", "ext", "allinurl"]
+		cmd = "CREATE TABLE IF NOT EXISTS"
+		tableform = "(id INTEGER PRIMARY KEY, content TEXT, firsttime TEXT, lasttime TEXT)"
+		for i in tablename:
+			self.cursor.execute(cmd+i+tableform)
+		self.conn.commit()
+
 	def insert(self):
-		
+		cmd = "INSERT INTO"
+
+	def closeHandle(self):
+		self.conn.commit()
+		self.conn.close()
 			
 
