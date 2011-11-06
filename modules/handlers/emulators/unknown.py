d09d0e7d05cf5edb167066861d95935765acf0fd
Lukas Rist <glaslos@gmail.com>
2011-11-06 20:48:23 +0000

    dork page test

--- modules/handlers/emulators/unknown.py
+++ modules/handlers/emulators/unknown.py
@@ -4,8 +4,8 @@ import codecs
 import sys
 
 if sys.getdefaultencoding() != 'utf-8':
-       reload(sys)
-       sys.setdefaultencoding('utf-8') # for the different python version unicode problem
+    reload(sys)
+    sys.setdefaultencoding('utf-8') # for the different python version unicode problem
 
 class DorkList(object):
     
@@ -14,7 +14,7 @@ class DorkList(object):
     
     def get_response(self):
         dork_page_list = os.listdir("modules/handlers/emulators/dork_list/pages")
-        shuffle(dork_page_list)
-        with codecs.open("modules/handlers/emulators/dork_list/pages/" + choice(dork_page_list), "r", "utf-8") as dork_page:
-            #return dork_page.read().decode('iso-8859-1')
+        #dork_page = choice(dork_page_list)
+        dork_page = "test"
+        with codecs.open("modules/handlers/emulators/dork_list/pages/" + dork_page, "r", "utf-8") as dork_page:
             return dork_page.read()
