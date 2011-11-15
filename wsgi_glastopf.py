47cc71c70f5e8ac1c187e5af79bb46e902f13eb0
Lukas Rist <glaslos@gmail.com>
2011-11-15 09:27:03 +0000

    Getting closer to a working wsgi module

--- wsgi_glastopf.py
+++ wsgi_glastopf.py
@@ -1,28 +1,41 @@
+# Copyright (C) 2011  Lukas Rist
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 import glastopf
 from urllib import quote
 
-def reconstruct_url(environ):
-    url = environ['wsgi.url_scheme']+'://'
-    if environ.get('HTTP_HOST'):
-        url += environ['HTTP_HOST']
-    else:
-        url += environ['SERVER_NAME']
-        if environ['wsgi.url_scheme'] == 'https':
-            if environ['SERVER_PORT'] != '443':
-                url += ':' + environ['SERVER_PORT']
-        else:
-            if environ['SERVER_PORT'] != '80':
-                url += ':' + environ['SERVER_PORT']
+def reconstruct_request(environ):
+    url = environ["REQUEST_METHOD"] + " "
     url += quote(environ.get('SCRIPT_NAME', ''))
     url += quote(environ.get('PATH_INFO', ''))
     if environ.get('QUERY_STRING'):
         url += '?' + environ['QUERY_STRING']
-    return url
+    url += " HTTP/1.1\r\n"
+    header = "Host: " + quote(environ.get('HTTP_HOST')) + "\r\n"
+    request = url + header + "\r\n\r\n"
+    return request
 
 def application(environ, start_response):
-    request = reconstruct_url(environ)
+    request = reconstruct_request(environ)
+    status = '200 OK' # HTTP Status
+    headers = [('Content-type', 'text/plain')] # HTTP Headers
+    start_response(status, headers)
     glastopf_honeypot = glastopf.GlastopfHoneypot()
-    response = glastopf_honeypot.handle_request(request, self.addr)
+    # FIXME: Provide the real requesting IP address
+    response = glastopf_honeypot.handle_request(request, addr = "127.0.0.1").split("\r\n\r\n")[1]
     return [response]
 
 if __name__ == '__main__':
