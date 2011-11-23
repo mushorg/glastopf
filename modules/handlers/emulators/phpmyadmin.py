424f648c78a9e1a882ee66d44584ef9c71cd9f99
Louis Liu <louisliu@locomotion.tw>
2011-11-23 06:42:33 +0000

    Now we can reply PHPMyadmin setup page.

--- modules/handlers/emulators/phpmyadmin.py
+++ modules/handlers/emulators/phpmyadmin.py
@@ -3,17 +3,15 @@ import hashlib
 import time
 
 class PMAEmulator(object):
-  def __init__(self):
-    pass
+    def __init__(self):
+        pass
 
-  def handle(self, attack_event):
-    pass
-#    On the way
-#    page = ""
-#    with open('modules/handlers/emulators/phpmyadmin/setup.php', 'r') as setup_php:
-#      page += setup_php.read()
-
-#    m = hashilb.md5()
-#    m.update("%d" % time.time() )
-#    attack_event.respond += page.unction.substitute( token = m.hexdigest())
-#    return attack_event
+    def handle(self, attack_event):
+        page = ""
+        with open('modules/handlers/emulators/phpmyadmin/script_setup.php', 'r') as setup_php:
+            page += setup_php.read()
+        m = hashlib.md5()
+        m.update("%d" % time.time() )
+        page_template = Template(page)
+        attack_event.response += page_template.substitute( token_value = m.hexdigest())
+        return attack_event
