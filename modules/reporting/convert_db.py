2e19c20cd2b20277c231c26b62e8537631368809
Guesslin <guesslin1986@gmail.com>
2011-12-09 16:40:33 +0000

    Version one convert_db.py script done

--- modules/reporting/convert_db.py
+++ modules/reporting/convert_db.py
@@ -1,2 +1,20 @@
 import sqlite3
 
+conn = sqlite3.connect("db/glastopf.db")
+cursor = conn.cursor()
+
+num_of_item = len(cursor.execute("SELECT * FROM events").fetchone())
+if num_of_item == 7:
+    try:
+        cursor.execute("ALTER TABLE events ADD COLUMN ")
+    except sqlite3.OperationalError, e:
+        print "Add Column Failed ", e
+    except sqlite3.ProgrammingError, e:
+        print "Add Column Failed " e
+    except Exception as e:
+        print "Unknow type ", e
+    finally:
+        conn.commit()
+
+cursor.close()
+conn.close()
