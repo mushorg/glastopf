c1b2d5cf9c3c528ee0a6e1a333d759838a97c057
Lukas Rist <glaslos@gmail.com>
2012-03-13 19:11:31 +0000

    bogus text for html selection fixed (Thanks Phani) code clean-up

--- modules/handlers/emulators/dork_list/gen_dork_list.py
+++ modules/handlers/emulators/dork_list/gen_dork_list.py
@@ -19,6 +19,7 @@ def prepare_text():
                 line_list.append(unicodedata.normalize('NFKD', text_line).encode('ascii', 'ignore'))
     return line_list
 
+
 def generate_dork_pages(first):
     if first:
         processor = process_dork_file.DorkFileProcessor()
@@ -30,14 +31,20 @@ def generate_dork_pages(first):
     intext_list = dork_reader.get_dork_list('intext')
     intitle_list = dork_reader.get_dork_list('intitle')
     shuffle(inurl_list)
-    while len(inurl_list) > 50:
+    while len(inurl_list) > 50 and len(line_list) > 50:
         body = ''
-        for i in range(0,49):
-            body += line_list[i] + " <a href='%s'>%s</a> " % (inurl_list.pop()[0], choice(intext_list)[0])
-        dork_page = gen_html.html_template(choice(intitle_list)[0], "http://www.google.com", body, "Epic Footer Powered By")
-        page_md5 = hashlib.md5(dork_page).hexdigest() 
+        for i in range(0, 49):
+            body += line_list.pop()
+            body += " <a href='%s'>%s</a> " % (inurl_list.pop()[0],
+                                               choice(intext_list)[0])
+        dork_page = gen_html.html_template(choice(intitle_list)[0],
+                                           "http://www.google.com",
+                                           body, 
+                                           "Epic Footer Powered By")
+        page_md5 = hashlib.md5(dork_page).hexdigest()
         with codecs.open("modules/handlers/emulators/dork_list/pages/%s" % page_md5, "w", "utf-8") as dork_file:
             dork_file.write(dork_page)
+
 
 def remove_old_dork_pages(old_dork_list):
     for file_path in old_dork_list:
@@ -45,6 +52,7 @@ def remove_old_dork_pages(old_dork_list):
             os.unlink(file_path)
         except Exception, e:
             print "Error deleting old dork pages:", e
+
 
 def get_old_dork_pages_list(folder):
     dork_lists = []
@@ -70,7 +78,8 @@ def regular_generate_dork(sleep_time):
         old_dork_list = get_old_dork_pages_list(dirname)
         generate_dork_pages(False)
         remove_old_dork_pages(old_dork_list)
-        
+
+
 def collect_dork(attack_event):
     if attack_event.matched_pattern != "unknown":
         dork_reader = dork_db.DorkDB()
@@ -81,4 +90,3 @@ def collect_dork(attack_event):
             print "parsed_request split error for '?':", e
         finally:
             dork_reader.closeHandle()
-
