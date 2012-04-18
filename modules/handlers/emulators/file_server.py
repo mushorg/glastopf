28152313bc857cce25a92231b9c0b135dc1aadc6
Lukas Rist <glaslos@gmail.com>
2012-04-18 15:32:39 +0000

    Some special case handling in the file server. TODO cleaned.

--- modules/handlers/emulators/file_server.py
+++ modules/handlers/emulators/file_server.py
@@ -8,8 +8,11 @@ class FileServer(object):
         pass
 
     def handle(self, attack_event):
+        server_path = 'modules/handlers/emulators/server_files'
         request_file = attack_event.parsed_request.url.lstrip('/')
-        with open(os.path.join('modules/handlers/emulators/server_files',
-                                request_file), 'r') as f:
-            attack_event.response += f.read()
+        if request_file == "":
+            request_file = "index.html"
+        if os.path.isfile(os.path.join(server_path, request_file)):
+            with open(os.path.join(server_path, request_file), 'r') as f:
+                attack_event.response += f.read()
         return attack_event
