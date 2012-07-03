e0345a1ca393b3f554325946e9a7ddb46a570eea
Phani Vadrevu <pvadrevu@gmail.com>
2012-07-03 19:25:31 +0000

    Corrected a bug in comments emulator. Slightly modified style.css and comments patterns to support more cases

--- modules/handlers/emulators/comments.py
+++ modules/handlers/emulators/comments.py
@@ -3,16 +3,22 @@ from random import choice
 import codecs
 from urlparse import parse_qs
 
+
 class CommentPoster(object):
     def __init__(self):
         pass
 
     def handle(self, attack_event):
-        dork_page_list = os.listdir("modules/handlers/emulators/dork_list/pages")
+        dork_page_list = os.listdir("modules/handlers/emulators/"
+                                    "dork_list/pages")
         if '.svn' in dork_page_list:
             dork_page_list.remove('.svn')
         dork_page = choice(dork_page_list)
-        with codecs.open("modules/handlers/emulators/dork_list/pages/" + 
+        with codecs.open("modules/handlers/emulators/dork_list/pages/" +
                             dork_page, "r", "utf-8") as dork_page:
-            comment = parse_qs(attack_event.parsed_request.body)['comment'][0]
-            attack_event.response = dork_page.read() + comment 
+            try:
+                comment = (parse_qs(attack_event.parsed_request.body)
+                                                      ['comment'][0])
+            except KeyError:
+                comment = ""
+            attack_event.response = dork_page.read() + comment
