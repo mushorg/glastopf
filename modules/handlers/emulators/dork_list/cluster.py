5ee9407b0accaddc517393b9e13b51ad6b499cb3
Johnny Vestergaard <jkv@unixcluster.dk>
2013-01-14 20:23:31 +0100

    configurable clustering parameters

--- modules/handlers/emulators/dork_list/cluster.py
+++ modules/handlers/emulators/dork_list/cluster.py
@@ -29,7 +29,9 @@ class Cluster(object):
         conf_parser.read(config)
         vectorizer = CountVectorizer(preprocessor=self.preprocessor, token_pattern=conf_parser.get('dork-db', 'token_pattern'))
         X = vectorizer.fit_transform(url_list)
-        km = KMeans(n_clusters=10, max_iter=50, verbose=0, n_init=20)
+        n_clusters = conf_parser.getint('dork-db', 'n_clusters')
+        max_iter = conf_parser.getint('dork-db', 'max_iter')
+        n_init = conf_parser.getint('dork-db', 'n_init')
+        km = KMeans(n_clusters=n_clusters, max_iter=max_iter, verbose=0, n_init=n_init, precompute_distances=True)
         km.fit(X)
         self.clusters = self.write_clusters(url_list, km.labels_)
-
