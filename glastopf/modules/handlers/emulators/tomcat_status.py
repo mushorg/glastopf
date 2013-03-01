9a3d89bf9629e58487c767868dee22b42e857d77
Johnny Vestergaard <jkv@unixcluster.dk>
2013-03-01 21:53:37 +0100

    fix #56

--- glastopf/modules/handlers/emulators/tomcat_status.py
+++ glastopf/modules/handlers/emulators/tomcat_status.py
@@ -23,7 +23,7 @@ class TomcatManagerStatusHandler(base_emulator.BaseEmulator):
         super(TomcatManagerStatusHandler, self).__init__(data_dir)
 
     def handle(self, attack_event):
-        tomcat_manager_path = os.path.join(self.data_dir, '/tomcat/manager_status.html')
+        tomcat_manager_path = os.path.join(self.data_dir, 'tomcat/manager_status.html')
         with open(tomcat_manager_path, 'r') as tomcat_manager_file:
             attack_event.response = tomcat_manager_file.read()
         return attack_event
