8a4f9274bb63b75335739bde368f0d12f42b2a86
glaslos <glaslos@gmail.com>
2013-03-10 10:38:45 +0100

    Adding jinja2 to setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     description='Web application honeypot',
     test_suite='nose.collector',
     install_requires=['gevent>=0.13.7', 'webob>=1.2.0', 'pyopenssl', 'chardet', 'sqlalchemy>=0.7.0',
-                      'lxml', 'beautifulsoup>=3.2.0', 'numpy>=1.6.1',
+                      'lxml', 'beautifulsoup>=3.2.0', 'numpy>=1.6.1', 'jinja2',
                       'scipy>=0.9.0', 'requests>=1.0.0', 'pymongo>=2.4',
                       'cssselect>=0.7.0', 'scikit_learn>=0.13.0', 'antlr_python_runtime'],
 )
