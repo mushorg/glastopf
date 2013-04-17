82c162de20e7a05ef503bae6db71836f3718b6c7
adarshdinesh <adarshdinesh@gmail.com>
2013-04-18 01:07:17 +0530

    Create test_virtual_files.py
    1) Run the function "randomize_vdocs()" twice.
    2) Take md5-sum of each file and compare it.

---
+++ glastopf/testing/test_virtual_files.py
@@ -0,0 +1,27 @@
+import unittest
+import hashlib
+import shutil
+import os
+
+from glastopf.glastopf import GlastopfHoneypot
+
+class TestVirtualFiles(unittest.TestCase):
+
+  def test_files(self):
+    v_files=('shadow', 'passwd', 'group' )
+  	f_dir1	= "/tmp/test1/"
+  	f_dir2	= "/tmp/test2/"
+  	os.makedirs(f_dir1)
+  	os.makedirs(f_dir2)
+
+  	randomize_vdocs(f_dir1)
+    randomize_vdocs(f_dir2)
+	  for v_file in v_files:
+        	dat_1  = open(os.path.join(f_dir1, "linux/etc/shadow"), 'r').read()
+        	dat_2  = open(os.path.join(f_dir2, "linux/etc/passwd"), 'r').read()
+		      md5_1  = hashlib.md5(dat_1).hexdigest()
+		      md5_2  = hashlib.md5(dat_2).hexdigest()
+		      self.assertNotEqual(md5_1,md5_2)
+	shutil.rmtree(f_dir1)
+	shutil.rmtree(f_dir2)
+
