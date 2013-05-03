25abe3aa685343f7ba4566e35e4f65ebf43e174b
Johnny Vestergaard <jkv@unixcluster.dk>
2013-05-03 21:16:19 +0200

    version bump
    close #104
    close #103

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Glastopf',
-    version='3.0.6',
+    version='3.0.7',
     packages=find_packages(exclude=['bin', 'testing', 'docs']),
     scripts=['bin/glastopf-runner'],
     url='http://glastopf.org',
