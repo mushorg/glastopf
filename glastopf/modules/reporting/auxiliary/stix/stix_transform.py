08390442578493ef2ea2522efcda430b4fef69b8
Johnny Vestergaard <jkv@unixcluster.dk>
2014-01-05 22:38:12 +0100

    test cases and a few fixes

--- glastopf/modules/reporting/auxiliary/stix/stix_transform.py
+++ glastopf/modules/reporting/auxiliary/stix/stix_transform.py
@@ -22,28 +22,32 @@ import os
 import jinja2
 import glastopf
 
+CAPEC_PATTERN_MAP = {'sqli': {'CAPEC-66': 'SQL Injection'},
+                     'rfi':  {'CAPEC-193': 'PHP Remote File Inclusion'},
+                     'lfi':  {'CAPEC-252': 'PHP Local File Inclusion'},
+                     # worst case for php_cgi_rce, we need better classification in modules
+                     'php_cgi_rce': {'CAPEC-193': 'PHP Remote File Inclusion'},
+                     'login': {'CAPEC-112': 'Brute force'}}
+
+# http headers accepted by cybox
+CYBOX_HEADERS = set(['Accept', 'Accept_Charset', 'Accept_Language', 'Accept_Datetime', 'Accept_Encoding',
+                     'Authorization', 'Cache_Control', 'Connection', 'Cookie', 'Content_Length', 'Content_MD5',
+                     'Content_Type', 'Date', 'Expect', 'From', 'Host', 'If_Match', 'If_Modified_Since', 'If_None_Match',
+                     'If_Range', 'If_Unmodified_Since', 'Max_Forwards', 'Pragma', 'Proxy_Authorization', 'Range',
+                     'Referer', 'TE', 'User_Agent', 'Via', 'Warning', 'DNT', 'X_Requested_With', 'X_Requested_For',
+                     'X_ATT_DeviceId', 'X_Wap_Profile'])
 
 class StixTransformer(object):
     def __init__(self, config):
         template_loader = jinja2.FileSystemLoader(searchpath=os.path.dirname(__file__))
-        template_env = jinja2.Environment(loader=template_loader)
+        template_env = jinja2.Environment(loader=template_loader, trim_blocks=True, lstrip_blocks=True)
         self.template = template_env.get_template('stix_glastopf_template.xml')
-
         self.config = config
-        self.capec_mapping = {'sqli': {'CAPEC-66': 'SQL Injection'},
-                              'rfi':  {'CAPEC-193': 'PHP Remote File Inclusion'},
-                              'lfi':  {'CAPEC-252': 'PHP Local File Inclusion'},
-                              # worst case for php_cgi_rce, we need better classification in modules
-                              'php_cgi_rce': {'CAPEC-193': 'PHP Remote File Inclusion'},
-                              'login': {'CAPEC-112': 'Brute force'}}
+
 
     def transform(self, event):
 
-        headers = []
-        for w in event.http_request.headers:
-            headers.append((w, event.http_request.headers.get(w, "")))
-
-        vars = {'package_id': str(uuid.uuid4()),
+        jinja_vars = {'package_id': str(uuid.uuid4()),
                 'namespace': 'Glastopf',
                 'namespace_uri': 'http://glastopf.org/stix-1',
                 'package_timestamp': datetime.utcnow().isoformat(),
@@ -62,15 +66,23 @@ class StixTransformer(object):
                 'http_path': event.http_request.request_path,
                 'http_body': event.http_request.request_body,
                 'http_raw_header': event.http_request.headers,
-                'http_parsed_header': headers,
+                'http_parsed_header': self._get_parsed_header(event),
                 'capec': self._pattern_to_capec(event)
         }
 
-        return self.template.render(vars)
+        return self.template.render(jinja_vars)
+
+    def _get_parsed_header(self, event):
+        parsed_header = []
+        for header_name in event.http_request.headers:
+            # make candidate header fit the cybox definitions
+            header_name_cap = header_name.title().replace('-', '_')
+            if header_name_cap in CYBOX_HEADERS:
+                parsed_header.append((header_name_cap, event.http_request.headers.get(header_name, "")))
+        return parsed_header
 
     def _pattern_to_capec(self, event):
-        print event.matched_pattern
-        if event.matched_pattern in self.capec_mapping:
-            return self.capec_mapping[event.matched_pattern]
+        if event.matched_pattern in CAPEC_PATTERN_MAP:
+            return CAPEC_PATTERN_MAP[event.matched_pattern]
         else:
             return {}
