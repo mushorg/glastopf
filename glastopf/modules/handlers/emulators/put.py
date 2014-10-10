24d399e331179a5aae9e774e98baa9b19068cb88
Raghvendra Mishra <raghvendramishr@gmail.com>
2014-10-10 11:31:31 +0530

    Create put.py
    Adding HTTP PUT method response code to 201(used for new file). I'm not adding HTTP code 204 because it is used for existing files which got modified by PUT.

---
+++ glastopf/modules/handlers/emulators/put.py
@@ -0,0 +1,8 @@
+from glastopf.modules.handlers import base_emulator
+
+
+class PUTRequest(base_emulator.BaseEmulator):
+  def __init__(self, data_dir):
+    super(PUTRequest, self).__init__(data_dir)
+  def handle(self, attack_event):
+    attack_event.set_response('', http_code=201)
