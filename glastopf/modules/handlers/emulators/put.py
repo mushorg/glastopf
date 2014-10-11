f7f0ead36c2af555f18968e61370d2f0f09279a2
Raghvendra Mishra <raghvendramishr@gmail.com>
2014-10-11 10:50:20 +0530

    Update put.py
    I have changed the response code to 200 because there is other mechanism is also required to send 201 code because it is used to differentiate between new and modified file on the server.

--- glastopf/modules/handlers/emulators/put.py
+++ glastopf/modules/handlers/emulators/put.py
@@ -2,7 +2,10 @@ from glastopf.modules.handlers import base_emulator
 
 
 class PUTRequest(base_emulator.BaseEmulator):
+  
   def __init__(self, data_dir):
+    
     super(PUTRequest, self).__init__(data_dir)
+    
   def handle(self, attack_event):
-    attack_event.set_response('', http_code=201)
+    attack_event.set_response('', http_code=200)
