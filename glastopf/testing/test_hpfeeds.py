38f45df19fe2feb1bb6e5a7624f960fe090538af
Colleen Blaho <cblaho@sas.upenn.edu>
2015-01-09 10:25:00 -0500

    enable hpfeeds for testing

--- glastopf/testing/test_hpfeeds.py
+++ glastopf/testing/test_hpfeeds.py
@@ -38,7 +38,7 @@ class Test_Loggers(unittest.TestCase):
         if os.path.isdir(self.tmpdir):
             shutil.rmtree(self.tmpdir)
 
-    @unittest.skip('disabled until honeycloud up and running again')
+    #@unittest.skip('disabled until honeycloud up and running again')
     def test_hpfeeds_event(self):
         """Objective: Testing if a basic event can be transmitted using hpfriends."""
 
@@ -57,7 +57,7 @@ class Test_Loggers(unittest.TestCase):
         error_message = logger.hpc.wait(2)
         self.assertIsNone(error_message)
 
-    @unittest.skip('disabled until honeycloud up and running again')
+    #@unittest.skip('disabled until honeycloud up and running again')
     def test_hpfeeds_event_with_file(self):
         """Objective: Testing if a event containing a file can be transmitted using hpfriends."""
 
